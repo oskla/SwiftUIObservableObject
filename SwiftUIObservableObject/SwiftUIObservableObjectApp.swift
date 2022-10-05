bdcd0c8c92d3e593dafb46f23de6e873b6b9bbce
oskla <oskarlarssson@gmail.com>
2022-10-05 13:11:40 +0200

    Initial Commit

---
+++ SwiftUIObservableObject/SwiftUIObservableObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIObservableObjectApp.swift
+//  SwiftUIObservableObject
+//
+//  Created by Oskar Larsson on 2022-10-05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIObservableObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
